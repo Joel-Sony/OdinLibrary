17160a6f1bc5112f3b513194ceedb2fdefe260bd
Joel <joelsony224@gmail.com>
2025-03-09 22:02:10 +0530

    books can now be added to webpage

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ closeButton.addEventListener("click", ()=>{
 
 let books = [];
 
+
 document.getElementById("bookForm").addEventListener("submit", function(event){
     event.preventDefault();
     const bookName = document.getElementById("bookName").value;
@@ -31,9 +32,10 @@ document.getElementById("bookForm").addEventListener("submit", function(event){
 
     let newBook = new Book(bookName,author,pages,read)
     books.push(newBook);
-    console.log(books);
     showPopup();
+    document.getElementById("bookForm").reset();
     dialog.close();
+    addBookToLibrary(newBook);
 })
 
 function showPopup(){
@@ -42,3 +44,38 @@ function showPopup(){
     setTimeout(() => popUp.classList.add("hidden"), 2000);
 }
 
+function addBookToLibrary(book) {
+    const bookContainer = document.createElement("div");
+    bookContainer.classList.add("bookContainer");
+
+    const bookList = document.createElement("ul");
+    bookList.classList.add("bookList");
+
+    const nameItem = document.createElement("li");
+    nameItem.textContent = `📖 Name: ${book.title}`;
+
+    const authorItem = document.createElement("li");
+    authorItem.textContent = `✍️ Author: ${book.author}`;
+
+    const pageItem = document.createElement("li");
+    pageItem.textContent = `📄 Pages: ${book.pages}`;
+
+    const readItem = document.createElement("li");
+    readItem.textContent = "Read: ";
+
+    const readCheckbox = document.createElement("input");
+    readCheckbox.type = "checkbox";
+    readCheckbox.checked = book.read;
+
+    readItem.appendChild(readCheckbox);
+
+
+    bookList.appendChild(nameItem);
+    bookList.appendChild(authorItem);
+    bookList.appendChild(pageItem);
+    bookList.appendChild(readItem);
+
+    bookContainer.appendChild(bookList);
+
+    document.getElementById("library").appendChild(bookContainer);
+}

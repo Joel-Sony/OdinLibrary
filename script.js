4cf8947bac41e4d073b819f2c3cdcc1f8f91b086
Joel <joelsony224@gmail.com>
2025-03-09 20:52:33 +0530

    Array to store books done

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-const addButton = document.querySelector(".add");
+class Book{
+    constructor(title,author,pages,read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+};
 
-addButton.addEventListener("click", ()=>{
-    let name = prompt("Enter name: ")
-    alert(name)
-} )
+const addButton = document.querySelector(".add");
+const dialog = document.getElementById("bookDialog");
+const closeButton = document.getElementById("closeDialog");
+
+
+addButton.addEventListener("click", function(event){
+    dialog.showModal();
+})
+
+closeButton.addEventListener("click", ()=>{
+    dialog.close();
+})
+
+let books = [];
+
+document.getElementById("bookForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    const bookName = document.getElementById("bookName").value;
+    const author = document.getElementById("authorName").value;
+    const pages = document.getElementById("numPages").value;
+    const read = document.getElementById("readStatus").checked;
+
+    let newBook = new Book(bookName,author,pages,read)
+    books.push(newBook);
+    console.log(books);
+    showPopup();
+    dialog.close();
+})
+
+function showPopup(){
+    let popUp = document.getElementById("popupMessage");
+    popUp.classList.remove("hidden");
+    setTimeout(() => popUp.classList.add("hidden"), 2000);
+}
+
